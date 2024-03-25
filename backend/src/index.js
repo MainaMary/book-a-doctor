59f1ea401b11f6889992de5110cd2b798c8040ce
Mary <marieshix98@gmail.com>
2024-03-25 17:08:57 +0300

    import missing modules

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,35 +1,36 @@
-import express from 'express'
-import mongoose from 'mongoose'
-import dotenv from 'dotenv'
-import authRoute from './routes/authRoutes'
-dotenv.config()
-const app = express()
-const port = process.env.PORT || 5000
-const corsOptions ={
-    origin :true
-}
-app.get('/',(req, res) =>{
-    res.send('Hello world')
-})
-//connect to DB
-const connectToDatabase = async()=>{
-    try{
-        await mongoose.connect(process.env.MONGO_URI,{
-            useNewUrlParser:true,
-            useUnifiedTopology:true
-        })
-        console.log('Database connection established')
-    }
-    catch(error){
-        console.log("MongoDB database connection failed")
-    }
-}
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cookeiParser from "cookie-parser";
+import cors from "cors";
+import authRoute from "./routes/authRoutes.js";
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 5000;
+const corsOptions = {
+  origin: true,
+};
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
+
 //middleware
-app.use(express.json())
-app.use(cookeParser())
-app.use(cors(corsOptions))
-app.use('api/v1/auth', authRoute)
-app.listen(port, () =>{
-    connectToDatabase()
-    console.log(`Server is running on port ${port}`)
-})
+app.use(express.json());
+app.use(cookeiParser());
+app.use(cors(corsOptions));
+app.use("api/v1/auth", authRoute);
+
+//connect to DB
+const connectToDatabase = async () => {
+  try {
+    await mongoose.connect(process.env.DATABASE_URL);
+    console.log("Database connection established");
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+app.listen(port, () => {
+  connectToDatabase();
+  console.log(`Server is running on port ${port}`);
+});
