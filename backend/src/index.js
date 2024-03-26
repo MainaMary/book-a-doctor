23a136af2124a0b4afd13d50f95661c247fafb0f
Mary <marieshix98@gmail.com>
2024-03-26 12:23:39 +0300

    update the routes base url

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,23 +6,28 @@ import cors from "cors";
 import morgan from "morgan";
 import authRoute from "./routes/authRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
+import doctorRoutes from "./routes/doctorRoutes.js";
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 5000;
 const corsOptions = {
   origin: true,
 };
-app.get("/", (req, res) => {
-  res.send("Hello world");
-});
 
 //middleware
 app.use(express.json());
 app.use(cookeiParser());
 app.use(cors(corsOptions));
-app.use("api/v1/auth", authRoute);
-app.use("api/v1/user", userRoutes);
-if (process.env === "development") {
+
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
+
+app.use("/auth", authRoute);
+app.use("/api/v1/user", userRoutes);
+app.use("/api/v1/doctor", doctorRoutes);
+
+if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
