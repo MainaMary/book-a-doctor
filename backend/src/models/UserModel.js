8b1f0f57d8b8ad7eabaa1ac876399698912b4855
Mary <marieshix98@gmail.com>
2024-03-26 12:26:10 +0300

    update the phone data type

--- backend/src/models/UserModel.js
+++ backend/src/models/UserModel.js
@@ -4,7 +4,7 @@ const UserModel = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   name: { type: String, required: true },
-  phone: { type: Number },
+  phone: { type: String },
   photo: { type: String },
   gender: { type: String, enum: ["male", "female", "other"] },
   bloodType: { type: String },
