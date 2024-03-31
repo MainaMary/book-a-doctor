5fcf9b2ff2a13769c8a652f883df20dd17d140c5
Mary <marieshix98@gmail.com>
2024-03-31 21:50:30 +0300

    set up constants file

---
+++ frontend/src/constants/index.ts
@@ -0,0 +1,14 @@
+export const footerData = {
+  partners: {
+    label: "Trusted By",
+    list: ["Enterprises", "Patients"],
+  },
+  services: {
+    label: "Services",
+    list: ["Mental Health", "Paedriatics treatment", "Labor and delivery"],
+  },
+  company: {
+    label: "Company",
+    list: ["About us", "Services", "Find a doctor", "Contact"],
+  },
+};
