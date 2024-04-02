4f492ca99f1316aa1ed1bca875f3e6d4b31794bc
Mary <marieshix98@gmail.com>
2024-04-02 23:44:49 +0300

    reusable input and label form components

--- frontend/src/components/form/input.tsx
+++ frontend/src/components/form/input.tsx
@@ -1,5 +1,27 @@
-const Input = () => {
-  return <div>input</div>;
+import { InputProps } from "../../types";
+const Input = ({
+  placeholder,
+  type,
+  name,
+  onChange,
+  id,
+  className = "",
+  value,
+}: InputProps) => {
+  return (
+    <input
+      placeholder={placeholder}
+      type={type}
+      id={id}
+      value={value}
+      name={name}
+      onChange={onChange}
+      className={`block w-full p-2 text-gray-900 border  rounded-lg bg-gray-50 text-base focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500
+       
+       ${className}`}
+    />
+  );
 };
 
 export default Input;
+//${errors ? "border-rose-400" : "border-gray-300"}
--- frontend/src/components/form/label.tsx
+++ frontend/src/components/form/label.tsx
@@ -1,5 +1,13 @@
-const Label = () => {
-  return <div>label</div>;
+import { LabelProps } from "../../types";
+const Label = ({ children }: LabelProps) => {
+  return (
+    <label
+      htmlFor="input"
+      className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+    >
+      {children}
+    </label>
+  );
 };
 
 export default Label;
