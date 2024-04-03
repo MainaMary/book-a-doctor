fa2788f0031552a1573ce42a91f25dc055f78869
Mary <marieshix98@gmail.com>
2024-04-03 12:59:20 +0300

    add register page

---
+++ frontend/src/pages/auth/register.tsx
@@ -0,0 +1,11 @@
+import AuthLayout from "../../components/auth/auth-layout";
+import RegisterForm from "../../components/auth/register-form";
+const Register = () => {
+  return (
+    <AuthLayout>
+      <RegisterForm />
+    </AuthLayout>
+  );
+};
+
+export default Register;
