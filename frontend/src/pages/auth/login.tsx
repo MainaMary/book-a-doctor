63ae2caac56dd5dce7a6950c0c38b5db90b797bf
Mary <marieshix98@gmail.com>
2024-04-03 12:58:54 +0300

    add login page

---
+++ frontend/src/pages/auth/login.tsx
@@ -0,0 +1,11 @@
+import LoginForm from "../../components/auth/login-form";
+import AuthLayout from "../../components/auth/auth-layout";
+const Login = () => {
+  return (
+    <AuthLayout>
+      <LoginForm />
+    </AuthLayout>
+  );
+};
+
+export default Login;
