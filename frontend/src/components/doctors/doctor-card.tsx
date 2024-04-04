82a71fc30fad4ed204bed2641d2225b77221f02d
Mary <marieshix98@gmail.com>
2024-04-04 23:36:32 +0300

    refactor image styling

--- frontend/src/components/doctors/doctor-card.tsx
+++ frontend/src/components/doctors/doctor-card.tsx
@@ -18,9 +18,9 @@ const DoctorCard = ({ doctor }: Props) => {
   const { name, photo, specilaization, totalPatients, avgRating, hospital } =
     doctor;
   return (
-    <div className="p-3 lg:p-5">
+    <div className="p-3 lg:p-5 border shadow-md">
       <div className="h-[400px] w-[350px]">
-        <img className="rounded-md w-full h-full" src={photo} alt={name} />
+        <img className="rounded-md w-auto h-auto" src={photo} alt={name} />
       </div>
       <h2 className="text-[12px] eading-30 lg:text-[16px] text-heading font-bold">
         {name}
