170e13d51845d863f280d24c43fdb5bcb1281e1c
Mary <marieshix98@gmail.com>
2024-04-05 19:35:11 +0300

    define method to upload image to cloudinary

--- frontend/src/utils/index.ts
+++ frontend/src/utils/index.ts
@@ -1,3 +1,29 @@
+import { BASE_URL } from "../constants/config";
+import axios from "axios";
+const uploadPreset = import.meta.env.VITE_UPLOAD_PRESET;
+const cloudName = import.meta.env.VITE_CLOUDINARY_NAME;
+
 export const getCurrentYear = () => {
   return new Date().getFullYear();
+};
+export const fileToDataString = (file: File) => {
+  return new Promise<string>((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onerror = (error) => reject(error);
+    reader.onload = () => resolve(reader.result as string);
+  });
+};
+export const uploadImageToCloudinary = async (file: File) => {
+  const formData = new FormData();
+  formData.append("file", file);
+  formData.append("upload_preset", uploadPreset);
+  formData.append("cloud_name", cloudName);
+
+  try {
+    const response = await axios.post(BASE_URL, formData);
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
 };
