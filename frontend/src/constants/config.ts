942b69e0d884a1c14ef6a147cf4ebd16fc93c210
Mary <marieshix98@gmail.com>
2024-04-07 01:23:04 +0300

    set up axios instance config

--- frontend/src/constants/config.ts
+++ frontend/src/constants/config.ts
@@ -1,4 +1,13 @@
-export const BASE_URL = `https://api.cloudinary.com/v1_1/${
+import axios, { AxiosInstance } from "axios";
+const BASE_URL = `https://api.cloudinary.com/v1_1/${
   import.meta.env.VITE_CLOUDINARY_NAME
 }/image/upload`;
-export const ADMIN_MODULE_URL = "https://localhost:5000";
+const ADMIN_MODULE_URL = "https://localhost:5000";
+const api: AxiosInstance = axios.create({
+  baseURL: ADMIN_MODULE_URL || "",
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
+});
+export { BASE_URL, ADMIN_MODULE_URL, api };
