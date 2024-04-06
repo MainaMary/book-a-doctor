4ca916881379ab2051b6727bc6b21a04e2fa6ff6
Mary <marieshix98@gmail.com>
2024-04-07 01:23:38 +0300

    wrap the app component with the query client provider

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,12 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Layout from "./layout/layout";
 function App() {
-  return <Layout />;
+  const queryClient = new QueryClient();
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Layout />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
