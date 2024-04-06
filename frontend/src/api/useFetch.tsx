3d914f615a37183e9acda54244541ac61aa09c4c
Mary <marieshix98@gmail.com>
2024-04-07 01:21:02 +0300

    define the api query function

---
+++ frontend/src/api/useFetch.tsx
@@ -0,0 +1,48 @@
+import { useQuery } from "@tanstack/react-query";
+import { api } from "../constants/config";
+
+export interface FetchResults {
+  isLoading: boolean;
+  isRefreshing: boolean;
+  isSuccess: boolean;
+  isError: boolean;
+  error: false | ErrorType | any;
+  data: any;
+  refetch?: () => void;
+}
+export type ErrorType = {
+  message?: string;
+  statusCode?: number;
+  errors: any;
+};
+
+export default function useFetch(name: any, path: string): FetchResults {
+  const {
+    isLoading,
+    isSuccess,
+    isError,
+    isFetching: isRefreshing,
+    data,
+    error,
+    refetch,
+  } = useQuery({
+    queryKey: name,
+    queryFn: async () => {
+      const response = await api.get(path);
+      return response.data;
+    },
+  });
+  return {
+    isLoading,
+    isRefreshing,
+    isSuccess,
+    isError,
+    data,
+    refetch,
+    error,
+    // error: {
+    //   message: error ? makeErrorMessage(error as AxiosError) : "",
+    //   statusCode: (error as AxiosError)?.response?.status || 0,
+    // },
+  };
+}
