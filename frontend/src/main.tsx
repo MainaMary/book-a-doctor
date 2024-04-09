600a38b541d84141811612c201df110f1871268d
Mary <marieshix98@gmail.com>
2024-04-09 23:53:49 +0300

    wrap the app component with the Authcontext provider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,11 +3,16 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import AuthContextProvider from "./context/useAuthContext.tsx";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <AuthContextProvider>
+        <App />
+        <ToastContainer />
+      </AuthContextProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
