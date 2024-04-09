6f26c81fcd386a7288ad1579ad78678edf865a50
Mary <marieshix98@gmail.com>
2024-04-09 23:49:55 +0300

    define custom error message for error handling

---
+++ frontend/src/api/error.ts
@@ -0,0 +1,28 @@
+import { AxiosError } from "axios";
+
+export const makeErrorMessage = (error: AxiosError) => {
+  try {
+    if (error.response?.status === 404) {
+      return error.response?.data.message || "Resource not found";
+    }
+
+    if (error.response?.status === 401) {
+      return "Not authorized";
+    }
+
+    if (error?.response?.data?.message) {
+      return error?.response?.data?.message;
+    }
+
+    if (error?.response?.data?.errors) {
+      return JSON.stringify(Object.values(error.response?.data?.errors)[0])
+        .replaceAll('"', "")
+        .replaceAll("[", "")
+        .replaceAll("]", "");
+    }
+
+    return `${error?.message}` || "Something went wrong";
+  } catch (err: any) {
+    return err;
+  }
+};
