41d39b3bb2198af3c9249cb62ca770d2b138b78b
Mary <marieshix98@gmail.com>
2024-04-10 15:26:46 +0300

    define authorization property in the headers object

--- frontend/src/constants/config.ts
+++ frontend/src/constants/config.ts
@@ -1,13 +1,21 @@
 import axios, { AxiosInstance } from "axios";
+import { getAccessToken } from "../utils/user";
 const BASE_URL = `https://api.cloudinary.com/v1_1/${
   import.meta.env.VITE_CLOUDINARY_NAME
 }/image/upload`;
 const ADMIN_MODULE_URL = "http://localhost:8000";
+
+const headers: HeadersInit = {
+  "Content-Type": "application/json",
+  Accept: "application/json",
+};
+const token = getAccessToken();
+if (token) {
+  headers["Authorization"] = `Bearer ${token}`;
+}
+
 const api: AxiosInstance = axios.create({
   baseURL: ADMIN_MODULE_URL,
-  headers: {
-    "Content-Type": "application/json",
-    Accept: "application/json",
-  },
+  headers,
 });
 export { BASE_URL, ADMIN_MODULE_URL, api };
