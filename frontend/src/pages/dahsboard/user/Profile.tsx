15ae01c86d14eb1702025d59a7f1da89c0602c3a
Mary <marieshix98@gmail.com>
2024-04-15 20:14:43 +0300

    invoke profile component

--- frontend/src/pages/dahsboard/user/Profile.tsx
+++ frontend/src/pages/dahsboard/user/Profile.tsx
@@ -1,5 +1,21 @@
+import { useAuth } from "../../../context/useAuthContext";
 const Profile = () => {
-  return <div>Profile</div>;
+  const {
+    userDetails: { data },
+  } = useAuth();
+
+  return (
+    <div className="px-6">
+      <div className="w-[100px] mb-2 h-[100px] border border-solid rounded-full">
+        <img src={data.photo} className="rounded-full w-auto cover  h-auto" />
+      </div>
+      <div className="space-y-3">
+        <p className="font-bold text-black text-3xl">{data.name}</p>
+        <p>{data.role}</p>
+        <p className="text-primary underline cursor-pointer">Edit profile</p>
+      </div>
+    </div>
+  );
 };
 
 export default Profile;
--- frontend/src/pages/dahsboard/user/UserDashboard.tsx
+++ frontend/src/pages/dahsboard/user/UserDashboard.tsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import { useAuth } from "../../../context/useAuthContext";
 import Appointments from "./Appointments";
 import Profile from "./Profile";
+import EditProfile from "./EditProfile";
+import { cn } from "../../../utils/classnames";
 const UserDashboard = () => {
   const [tabs, setTabs] = useState("appointments");
   const { userDetails } = useAuth();
@@ -20,22 +22,29 @@ const UserDashboard = () => {
     setTabs(name);
   };
   return (
-    <div className="container my-8 grid grid-cols-2">
+    <div className="container my-8 flex ">
       <div className="w-[70%]">
-        <p>{userDetails?.data?.name}</p>
-        <ul className="flex gap-6 border-b-2 w-full border-solid">
+        <p className="font-bold text-3xl">{userDetails?.data?.name}</p>
+        <ul className="flex gap-6 border-b-3 w-[400px] border-gray-300 ">
           {list.map((label) => (
             <li
-              className="cursor-pointer"
+              className={cn(
+                "cursor-pointer",
+                label.name === tabs
+                  ? "text-primary border-b-[1px] border-primary"
+                  : " "
+              )}
               onClick={() => handleTab(label.name)}
             >
               {label.label}
             </li>
           ))}
         </ul>
-        {tabs === "appointments" ? <Appointments /> : <Profile />}
+        {tabs === "appointments" ? <Appointments /> : <EditProfile />}
       </div>
-      <div className="w-[30%]">The profile</div>
+      <div className="w-[30%] border-l-[1px] border-gray-300">
+        <Profile />
+      </div>
     </div>
   );
 };
