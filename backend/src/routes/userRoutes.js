d2e6f20c603dc756f82e5ff828c8347287aee2dc
Mary <marieshix98@gmail.com>
2024-04-22 23:59:19 +0300

    define getuser profile endpoint

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -10,7 +10,8 @@ import {
 const router = express.Router();
 router.route("/:id").get(getUser).put(updateUser).delete(deleteUser);
 router.route("/").get(getAllUsers);
-router.get("/profile/user.:id", getUserProfile);
+router.route("/").get(getUserProfile);
+// router.get("/profile/user/:id", getUserProfile);
 router.get("appointments/my-appointments/:id", getUserAppointments);
 
 export default router;
