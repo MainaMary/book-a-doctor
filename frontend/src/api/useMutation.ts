37143ef3030ece21cdc39eb1f3cbfc6864f4c98c
Mary <marieshix98@gmail.com>
2024-06-05 23:27:41 +0300

    edit  custom hook mutations

--- frontend/src/api/useMutation.ts
+++ frontend/src/api/useMutation.ts
@@ -1,99 +1,103 @@
-import { useMutation } from "@tanstack/react-query";
+import { UseMutationOptions, useMutation } from "@tanstack/react-query";
 import { api } from "../constants/config";
 import { AxiosError, AxiosResponse } from "axios";
 import { makeErrorMessage } from "./error";
 
-export default function usePostRequest(
+export const usePostRequest = <T, _unknown>(
   path: string,
-  payload: any,
-  onSuccess: (data?: any) => void,
-  onFailure: (error?: any) => void
-) {
-  const postRequest = async () => {
-    try {
+  payload: string,
+  onSuccess: (x: any) => void,
+  onError: (x: any) => void,
+  options?: UseMutationOptions<T, unknown, unknown>
+) => {
+  return useMutation({
+    mutationFn: async () => {
       const response = await api.post(path, payload);
-      return response;
-    } catch (error) {
-      throw error;
-    }
-  };
-  const mutation = useMutation({
-    postRequest,
+      return response.data;
+    },
     onSuccess: (data) => {
       onSuccess(data);
     },
     onError: (error: AxiosError) => {
-      onFailure({
-        message: makeErrorMessage(error),
+      onError({
+        message: "",
         statusCode: error.response?.status,
-        errors: error.response?.data,
+        errors: error?.response?.data,
       });
     },
+    ...options,
   });
-  return mutation;
-}
+};
+// export default function usePostRequest(
+//   path: string,
+//   payload: any,
+//   onSuccess: (data?: any) => void,
+//   onFailure: (error?: any) => void
+// ) {
+//   const postRequest = async () => {
+//     try {
+//       const response = await api.post(path, payload);
+//       return response;
+//     } catch (error) {
+//       throw error;
+//     }
+//   };
+//   const mutation = useMutation({
+//     postRequest,
+//     onSuccess: (data) => {
+//       onSuccess(data);
+//     },
+//     onError: (error: AxiosError) => {
+//       onFailure({
+//         message: makeErrorMessage(error),
+//         statusCode: error.response?.status,
+//         errors: error.response?.data,
+//       });
+//     },
+//   });
+//   return mutation;
+// }
 
-export function usePutRequest(
-  path: string,
-  payload: any,
-  onSuccess: (data?: any) => void,
-  onFailure: (error?: any) => void
-) {
-  const putRequest = () => api.put(path, payload);
-  const mutation = useMutation({
-    putRequest,
-    onSuccess: (data) => {
-      onSuccess(data);
-    },
-    onError: (error: AxiosError) => {
-      onFailure({
-        message: makeErrorMessage(error),
-        statusCode: error?.response?.status,
-        errors: error.response?.data,
-      });
-    },
-  });
-  return mutation;
-}
-export function usePatchRequest(
-  path: string,
-  payload: any,
-  onSuccess: (data?: any) => void,
-  onFailure: (error?: any) => void
-) {
-  const patchRequest = async () => await api.patch(path, payload);
-  const mutation = useMutation({
-    patchRequest,
-    onSuccess: (data) => {
-      onSuccess(data);
-    },
-    onError: (error: AxiosError) => {
-      onFailure({
-        message: makeErrorMessage(error),
-        statusCode: error.response?.status,
-      });
-    },
-  });
-  return mutation;
-}
-
-export function useDeleteRequest(
-  path: string,
-  onSuccess: (data?: any) => void,
-  onFailure: (error?: any) => void
-) {
-  const deleteRequest = () => api.delete(path);
-  const mutation = useMutation({
-    deleteRequest,
-    onSuccess: (data) => {
-      onSuccess(data);
-    },
-    onError: (error: AxiosError) => {
-      onFailure({
-        message: makeErrorMessage(error),
-        statusCode: error.response?.status,
-      });
-    },
-  });
-  return mutation;
-}
+// export function usePutRequest(
+//   path: string,
+//   payload: any,
+//   onSuccess: (data?: any) => void,
+//   onFailure: (error?: any) => void
+// ) {
+//   const putRequest = () => api.put(path, payload);
+//   const mutation = useMutation({
+//     putRequest,
+//     onSuccess: (data) => {
+//       onSuccess(data);
+//     },
+//     onError: (error: AxiosError) => {
+//       onFailure({
+//         message: makeErrorMessage(error),
+//         statusCode: error?.response?.status,
+//         errors: error.response?.data,
+//       });
+//     },
+//   });
+//   return mutation;
+// }
+// export function usePatchRequest(
+//   path: string,
+//   payload: any,
+//   onSuccess: (data?: any) => void,
+//   onFailure: (error?: any) => void
+// ) {
+//   const patchRequest = async () => await api.patch(path, payload);
+//   const mutation = useMutation({
+//     patchRequest,
+//     onSuccess: (data) => {
+//       onSuccess(data);
+//     },
+//     onError: (error: AxiosError) => {
+//       onFailure({
+//         message: makeErrorMessage(error),
+//         statusCode: error.response?.status,
+//       });
+//     },
+//   });
+//   return mutation;
+// }
