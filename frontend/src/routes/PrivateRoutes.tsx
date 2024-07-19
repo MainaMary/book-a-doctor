98515d5d79f030cdd3983fb6bb3e03883d7d7714
Mary <marieshix98@gmail.com>
2024-07-19 23:54:32 +0300

    handle private routes configuration

--- frontend/src/routes/PrivateRoutes.tsx
+++ frontend/src/routes/PrivateRoutes.tsx
@@ -2,6 +2,7 @@ import { getAccessToken } from "../utils/user";
 import { Outlet, Navigate } from "react-router-dom";
 const PrivateRoutes = () => {
   const token = getAccessToken();
+  console.log({ token });
   return <div>{token ? <Outlet /> : <Navigate to={"/auth/login"} />}</div>;
 };
 
