dfb7ad7af52c51985f9a5a495c609872fa02eb4b
Mary <marieshix98@gmail.com>
2024-07-19 23:55:22 +0300

    update doctors dahsboard

--- frontend/src/pages/dahsboard/doctor/DoctorDashboard.tsx
+++ frontend/src/pages/dahsboard/doctor/DoctorDashboard.tsx
@@ -1,5 +1,50 @@
+import { useState } from "react";
+import DoctorProfile from "./DoctorProfile";
+import Reviews from "./Reviews";
+import { useAuth } from "../../../context/useAuthContext";
+import { cn } from "../../../utils/classnames";
 const DoctorDashboard = () => {
-  return <div className="container">DoctorDashboard</div>;
+  const [tabs, setTabs] = useState("profile");
+  const {
+    userDetails: { data },
+  } = useAuth();
+  const list = [
+    {
+      name: "profile",
+      label: "Profile",
+    },
+    {
+      name: "reviews",
+      label: "Reviews",
+    },
+  ];
+  const handleTabNavigation = (name: string) => {
+    setTabs(name);
+  };
+  return (
+    <div className="container my-8 flex">
+      <div className="w-[70%]">
+        <p className="font-bold text-3xl">{data?.name}</p>
+        <ul className="flex gap-6 border-b-3 w-[400px] border-gray-300 ">
+          {list.map((label) => (
+            <li
+              className={cn(
+                "cursor-pointer",
+                label.name === tabs
+                  ? "text-primary border-b-[1px] border-primary"
+                  : " "
+              )}
+              onClick={() => handleTabNavigation(label.name)}
+            >
+              {label.label}
+            </li>
+          ))}
+        </ul>
+        {tabs === "profile" ? <DoctorProfile /> : <Reviews />}
+      </div>
+      <div className="w-[30%] border-l-[1px] border-gray-300">hfehg</div>
+    </div>
+  );
 };
 
 export default DoctorDashboard;
